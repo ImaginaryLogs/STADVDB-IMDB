16458fed9767183e6eb99aeb9db80e784f3b16fc
PyroRSC <109332014+PyroRSC@users.noreply.github.com>
2025-10-17 23:35:14 +0800

    dum dum

--- scripts/dummy_data_table.sql
+++ scripts/dummy_data_table.sql
@@ -1,65 +1,56 @@
--- Table: title_akas
-CREATE TABLE title_akas (
-    titleId TEXT NOT NULL,
-    ordering INT NOT NULL,
-    title TEXT,
-    region TEXT,
-    language TEXT,
-    types TEXT,           -- Array of enumerated attributes
-    attributes TEXT,      -- Array of additional terms
-    isOriginalTitle BOOLEAN,
-    PRIMARY KEY (titleId, ordering)
-);
+DROP SCHEMA IF EXISTS imdb_source;
+CREATE SCHEMA imdb_source;
+use imdb_source;
 
 -- Table: title_basics
 CREATE TABLE title_basics (
-    tconst TEXT PRIMARY KEY,
+    tconst VARCHAR(100) PRIMARY KEY,
     titleType TEXT,
     primaryTitle TEXT,
     originalTitle TEXT,
-    isAdult BOOLEAN,
+    isAdult TEXT,
     startYear TEXT,       -- YYYY format
     endYear TEXT,         -- YYYY format or '\N'
-    runtimeMinutes INT,
+    runtimeMinutes TEXT,
     genres TEXT            -- Array of up to three genres
 );
 
 -- Table: title_crew
 CREATE TABLE title_crew (
-    tconst TEXT PRIMARY KEY,
+    tconst VARCHAR(100) PRIMARY KEY,
     directors TEXT,        -- Array of nconsts
     writers TEXT           -- Array of nconsts
 );
 
 -- Table: title_episode
 CREATE TABLE title_episode (
-    tconst TEXT PRIMARY KEY,
-    parentTconst TEXT,
-    seasonNumber INT,
-    episodeNumber INT
+    tconst VARCHAR(100) PRIMARY KEY,
+    parenttconst TEXT,
+    seasonNumber TEXT,
+    episodeNumber TEXT
 );
 
 -- Table: title_principals
 CREATE TABLE title_principals (
-    tconst TEXT NOT NULL,
-    ordering INT NOT NULL,
+    tconst VARCHAR(100) NOT NULL,
+    ordering VARCHAR(64) NOT NULL,
     nconst TEXT NOT NULL,
     category TEXT,
     job TEXT,
     characters TEXT,
-    PRIMARY KEY (tconst, ordering)
+    PRIMARY KEY(tconst,ordering)
 );
 
 -- Table: title_ratings
 CREATE TABLE title_ratings (
-    tconst TEXT PRIMARY KEY,
-    averageRating FLOAT,  
-    numVotes INT,
+    tconst VARCHAR(100) PRIMARY KEY,
+    averageRating TEXT,  
+    numVotes TEXT
 );
 
 -- Table: name_basics
 CREATE TABLE name_basics (
-    nconst TEXT PRIMARY KEY,
+    nconst VARCHAR(100) PRIMARY KEY,
     primaryName TEXT,
     birthYear TEXT,              -- YYYY format
     deathYear TEXT,              -- YYYY format or '\N'
